5af4e4efc00dbe6418ed7d4144722465f4104122
Raquel S <151413335+psrachs@users.noreply.github.com>
2024-06-26 15:42:42 -0300

    Add files via upload

--- stitchstyle-main/Stitch Style/index.js
+++ stitchstyle-main/Stitch Style/index.js
@@ -3,34 +3,62 @@ const btnPrev = document.getElementById('prev-button');
 const btnNext = document.getElementById('next-button');
 
 let currentSlide = 0;
+let autoSlideInterval; 
 
 function hideSlider() {
-slider.forEach(item => item.classList.remove('on'))
+  slider.forEach(item => item.classList.remove('on'));
 }
 
 function showSlider() {
-  slider[currentSlide].classList.add('on')
+  slider[currentSlide].classList.add('on');
 }
 
-function nextSlider(){
-  hideSlider()
-  if(currentSlide == slider.length -1) {
-    currentSlide = 0
+function nextSlider() {
+  hideSlider();
+  if (currentSlide === slider.length - 1) {
+    currentSlide = 0;
   } else {
-    currentSlide++
+    currentSlide++;
   }
-  showSlider()
+  showSlider();
 }
 
-function prevSlider(){
-  hideSlider()
-  if(currentSlide == 0) {
-    currentSlide = slider.length -1
+function prevSlider() {
+  hideSlider();
+  if (currentSlide === 0) {
+    currentSlide = slider.length - 1;
   } else {
-    currentSlide--
+    currentSlide--;
   }
-  showSlider()
+  showSlider();
 }
 
-btnNext.addEventListener('click', nextSlider)
-btnPrev.addEventListener('click', prevSlider)
+function startAutoSlide() {
+  autoSlideInterval = setInterval(nextSlider, 4500); 
+}
+
+function stopAutoSlide() {
+  clearInterval(autoSlideInterval);
+}
+
+btnNext.addEventListener('click', () => {
+  nextSlider();
+  stopAutoSlide(); 
+});
+
+btnPrev.addEventListener('click', () => {
+  prevSlider();
+  stopAutoSlide(); 
+});
+
+
+startAutoSlide();
+
+
+btnNext.addEventListener('mouseover', stopAutoSlide);
+btnPrev.addEventListener('mouseover', stopAutoSlide);
+
+
+btnNext.addEventListener('mouseout', startAutoSlide);
+btnPrev.addEventListener('mouseout', startAutoSlide);
+
